5626a801a0af9da173bf5ac906cdbb469b589e29
Murat Eken <meken@google.com>
2022-07-04 16:31:58 +0200

    cleaning up...

--- build/start-pipelines.sh
+++ build/start-pipelines.sh
@@ -7,7 +7,7 @@ SECRET_NAMES=`gcloud secrets list --format="value(name)"`
 for SECRET in $SECRET_NAMES; do
     SECRET_VALUE=`gcloud secrets versions access latest --secret=$SECRET`
     BODY=$(jq -n --arg data "$SECRET_VALUE" '$ARGS.named')
-    curl -X PUT -H "Authorization: Bearer ${AUTH_TOKEN}" -d "$BODY" \
+    curl -s -X PUT -H "Authorization: Bearer ${AUTH_TOKEN}" -d "$BODY" \
        "${CDAP_ENDPOINT}/v3/namespaces/default/securekeys/${SECRET}"
 done
 
@@ -16,6 +16,6 @@ PIPELINES=`curl -s -H "Authorization: Bearer ${AUTH_TOKEN}" \
     "${CDAP_ENDPOINT}/v3/namespaces/default/apps?artifactName=cdap-data-pipeline" | jq -r '.[] | .name'`
 
 for PIPELINE in $PIPELINES; do
-    curl -X POST -H "Authorization: Bearer ${AUTH_TOKEN}" \
+    curl -s -X POST -H "Authorization: Bearer ${AUTH_TOKEN}" \
         "${CDAP_ENDPOINT}/v3/namespaces/default/apps/${PIPELINE}/workflows/DataPipelineWorkflow/start"
 done
